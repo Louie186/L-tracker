6ed90721c1663dd75252d346ae3b6fa53f85f709
Louie186 <louieorinia@gmail.com>
2026-06-06 14:02:28 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -20,11 +20,14 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {
   if (e.request.method !== "GET") return;
+  if (!e.request.url.startsWith("http")) return;
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
       return res;
-    }))
+    }).catch(() => cached))
   );
 });
